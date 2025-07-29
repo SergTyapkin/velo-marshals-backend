e1e85ffeaed2ff2a1840b165144deeae84e2df7e
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-07-29 18:39:43 +0300

    add ON UPDATE CASCADE

--- configs/init.sql
+++ configs/init.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
-    userId   SERIAL NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    userId   SERIAL NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     token    TEXT NOT NULL UNIQUE,
     expires  TIMESTAMP WITH TIME ZONE,
     ip          TEXT,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS sessions (
 
 CREATE TABLE IF NOT EXISTS secretCodes (
     id             SERIAL PRIMARY KEY,
-    userId         INT REFERENCES users(id) ON DELETE CASCADE,
+    userId         INT REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     code           TEXT NOT NULL UNIQUE,
     type           TEXT NOT NULL,
     expires        TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -44,24 +44,25 @@ CREATE TABLE IF NOT EXISTS secretCodes (
 
 ------ Events data -------
 CREATE TABLE IF NOT EXISTS events (
-    id               SERIAL PRIMARY KEY,
-    title            TEXT NOT NULL DEFAULT '',
-    description      TEXT NOT NULL DEFAULT '',
-    routeDescription TEXT NOT NULL DEFAULT '',
-    createdDate      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    startDate        TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NULL,
-    cameDate         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NULL,
-    previewUrl       TEXT DEFAULT NULL,
-    customCSS        TEXT DEFAULT NULL,
-    lapDistanceKm    FLOAT NOT NULL,
-    medalPreviewUrl  TEXT DEFAULT NULL,
-    authorId         INT REFERENCES users(id) ON DELETE SET NULL
+    id                   SERIAL PRIMARY KEY,
+    title                TEXT NOT NULL DEFAULT '',
+    description          TEXT NOT NULL DEFAULT '',
+    routeDescription     TEXT NOT NULL DEFAULT '',
+    createdDate          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    startDate            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NULL,
+    cameDate             TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NULL,
+    previewUrl           TEXT DEFAULT NULL,
+    customCSS            TEXT DEFAULT NULL,
+    lapDistanceKm        FLOAT NOT NULL,
+    medalPreviewUrl      TEXT DEFAULT NULL,
+    isRegistrationOpened BOOLEAN NOT NULL DEFAULT FALSE,
+    authorId             INT REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS registrations (
     id           SERIAL PRIMARY KEY,
-    userId       INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    eventId      INT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+    userId       INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    eventId      INT NOT NULL REFERENCES events(id) ON DELETE CASCADE ON UPDATE CASCADE,
     isConfirmed  BOOLEAN DEFAULT NULL,
     userComment  TEXT DEFAULT NULL,
     adminComment TEXT DEFAULT NULL,
@@ -82,13 +83,13 @@ CREATE TABLE IF NOT EXISTS equipment (
     previewUrl      TEXT DEFAULT NULL,
     amountTotal     INT NOT NULL,
     isNeedsToReturn BOOLEAN NOT NULL,
-    eventId         INT NOT NULL REFERENCES events(id) ON DELETE CASCADE
+    eventId         INT NOT NULL REFERENCES events(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS usersEquipments (
     id             SERIAL PRIMARY KEY,
-    userId         INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    equipmentId    INT NOT NULL REFERENCES equipment(id) ON DELETE CASCADE,
+    userId         INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    equipmentId    INT NOT NULL REFERENCES equipment(id) ON DELETE CASCADE ON UPDATE CASCADE,
     amountHolds    INT NOT NULL,
     takenDate      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updatedDate    TIMESTAMP WITH TIME ZONE DEFAULT NULL,
@@ -103,16 +104,16 @@ CREATE TABLE IF NOT EXISTS achievements (
     levels         INT NOT NULL,
     previewUrl     TEXT DEFAULT NULL,
     isSpecial      BOOLEAN NOT NULL DEFAULT FALSE,
-    authorId       INT REFERENCES users(id) ON DELETE SET NULL
+    authorId       INT REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS usersAchievements (
     id             SERIAL PRIMARY KEY,
-    userId         INT REFERENCES users(id) ON DELETE CASCADE,
-    achievementId  INT REFERENCES achievements(id) ON DELETE CASCADE,
+    userId         INT REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    achievementId  INT REFERENCES achievements(id) ON DELETE CASCADE ON UPDATE CASCADE,
     level          INT NOT NULL DEFAULT 1,
     gottenDate     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    authorId       INT REFERENCES users(id) ON DELETE SET NULL,
+    authorId       INT REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
     UNIQUE (userId, achievementId)
 );
 
@@ -123,14 +124,14 @@ CREATE TABLE IF NOT EXISTS docs (
     text           TEXT NOT NULL,
     createdDate    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     editedDate     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    authorId       INT REFERENCES users(id) ON DELETE SET NULL,
-    lastRedactorId INT REFERENCES users(id) ON DELETE SET NULL
+    authorId       INT REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    lastRedactorId INT REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 ------ Images ------
 CREATE TABLE IF NOT EXISTS images (
     id             SERIAL PRIMARY KEY,
-    author         INT REFERENCES users(id) ON DELETE SET NULL,
+    author         INT REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
     type           TEXT NOT NULL,
     bytes          BYTEA
 );
@@ -138,7 +139,7 @@ CREATE TABLE IF NOT EXISTS images (
 ------ Total history ------
 CREATE TABLE IF NOT EXISTS history (
     id             SERIAL PRIMARY KEY,
-    userId         INT REFERENCES users(id) ON DELETE SET NULL,
+    userId         INT REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
     type           TEXT NOT NULL,
     text           TEXT NOT NULL,
     date           TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
